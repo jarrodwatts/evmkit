eb8d505eadb0f0d13951ddba5095c89423bca408
jarrodwatts <jarrod@thirdweb.com>
2023-05-22 14:54:19 +1000

    Add option to have private key in auth in env variables

--- template/application/pages/api/auth/[...thirdweb].ts
+++ template/application/pages/api/auth/[...thirdweb].ts
@@ -1,10 +1,15 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
-import { EthersWallet } from "@thirdweb-dev/wallets";
+import { EthersWallet, PrivateKeyWallet } from "@thirdweb-dev/wallets";
 import { ethers } from "ethers";
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
+  // Use the domain from the environment or default to evmkit.com
   domain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || "evmkit.com",
-  wallet: new EthersWallet(ethers.Wallet.createRandom()),
+
+  // Use the private key from the environment or generate a random one
+  wallet: process.env.THIRDWEB_AUTH_PRIVATE_KEY
+    ? new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY)
+    : new EthersWallet(ethers.Wallet.createRandom()),
 });
 
 export default ThirdwebAuthHandler();
