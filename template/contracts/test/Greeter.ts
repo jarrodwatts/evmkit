456e9840022ceca105d66e1028221f399d6af2f9
jarrodwatts <jarrod@thirdweb.com>
2023-05-26 15:39:30 +1000

    Change name of testsuite

--- template/contracts/test/Greeter.ts
+++ template/contracts/test/Greeter.ts
@@ -2,7 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("Greeter", function () {
   // Define a fixture to reuse the same setup in every test
   async function deployFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
