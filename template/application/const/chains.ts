345bd12e4684f3550c3cbc40e2865f28db16c25f
Jarrod Watts <jarrodwatts16@gmail.com>
2023-08-01 14:40:33 +1000

    Use Mumbai in production_chain

--- template/application/const/chains.ts
+++ template/application/const/chains.ts
@@ -10,6 +10,6 @@ import { Polygon, Mumbai } from "@thirdweb-dev/chains";
 export const IS_DEV_ENV = process.env.NODE_ENV === "development";
 
 const DEVELOPMENT_CHAIN = Mumbai; // e.g. Mumbai used for local development
-const PRODUCTION_CHAIN = Polygon; // e.g. Polygon used for production
+const PRODUCTION_CHAIN = Mumbai; // You can use a different chain for production (e.g. Polygon)
 
 export const CHAIN = IS_DEV_ENV ? DEVELOPMENT_CHAIN : PRODUCTION_CHAIN;
